bc94ba42f18da0679a6dc80532753dd97c6abd74
aandresrp <andresulatina@gmail.com>
2017-08-11 11:06:15 -0600

    Versión 0.1
    Primera versión

---
+++ Model/Dominio/Repositorio/Articulo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Ulatina.Topicos.GenericRepository;
+
+namespace Model.Dominio.Repositorio
+{
+    internal class Articulo
+    {
+        private static DbContext miContexto;
+
+        private readonly EntityFrameworkRepository<DbContext> _Repository;
+
+        private readonly string _includeTables = "Articulo, EstadoArticulo, Permiso, Persona";
+        private readonly string _noIncludeTables = string.Empty;
+
+        public Articulo()
+        {
+            miContexto = new ProyectoFinalEntities();
+            _Repository = NewMethod();
+        }
+
+        private static EntityFrameworkRepository<DbContext> NewMethod()
+        {
+            return new EntityFrameworkRepository<DbContext>(miContexto);
+        }
+
+        public Articulo(DbContext elContexto)
+        {
+            miContexto = elContexto;
+            _Repository = new EntityFrameworkRepository<DbContext>(miContexto);
+        }
+
+        internal IList<Articulo> ConsultaPorNombreDeProducto(string elNombreDelProducto)
+        {
+            // para usar el .ToList(), se requiere haber incluido el System.Linq;
+            var losProductos = _Repository.Get<Articulo>(p => p.Name.Contains(elNombreDelProducto), null, _includeTables, null, null).ToList();
+            return losProductos;
+        }
+
+        internal Articulo ConsultaProductosPorNumero(string miNumeroDeProducto)
+        {
+            var elArticulo = _Repository.GetOne<Articulo>(p => p.ProductNumber.Equals(miNumeroDeProducto), _includeTables);
+            return elArticulo;
+        }
+
+        internal IList<Product> ListarProductos()
+        {
+            // para usar el .ToList(), se requiere haber incluido el System.Linq;
+            var losProductos = _Repository.GetAll<Product>(null, _includeTables, null, null).ToList();
+            return losProductos;
+        }
+    }
+}
